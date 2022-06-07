74c18ea29885d55e2b273b55a3a9fca65a177099
Pratikmamdapure <85956467+Pratikmamdapure@users.noreply.github.com>
2022-06-07 21:01:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link href="./game.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia&effect=fire">
+    <title>Home</title>
+</head>
+<body>
+    <div class=box>
+        <h1>Classic Snake</h1>
+    </div>
+    
+    <div class="home">
+    
+    <img src="./cta_alt.png">
+    <div class="play_button">
+    <button class="button button1" onclick="window.location.href='game.php';">Play</button>
+    <button class="button button1">More Games</button>
+    </div>
+</div>
+</body>
+</html>
